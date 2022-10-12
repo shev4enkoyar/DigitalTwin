fa161c454ba1a4a0099e2d9d0eca1d8660bae2e8
Arkirka <denis.vorobyov.00@gmail.com>
2022-10-12 23:07:46 +0300

    layout refactored

--- WebClient/ClientApp/src/components/layout/Layout.js
+++ WebClient/ClientApp/src/components/layout/Layout.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import { NavMenu } from './navMenu/NavMenu';
-
+import { NavMenu } from '../navMenu/NavMenu';
 export class Layout extends Component {
   static displayName = Layout.name;
 
   render () {
     return (
-      <div>
+      <div style={{height: "100%"}} >
         <NavMenu />
-        <>
+
+        <div style={{height: "calc(100% - 56px)"}}>
           {this.props.children}
-        </>
+        </div>
       </div>
     );
   }
