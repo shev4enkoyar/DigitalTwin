a1669ee917a79180e04338dfd1c7fdb6e7acfb06
Arkirka <denis.vorobyov.00@gmail.com>
2022-10-12 23:08:44 +0300

    profile dropdown button added

--- WebClient/ClientApp/src/components/navMenu/components/ProfileDropDown.js
+++ WebClient/ClientApp/src/components/navMenu/components/ProfileDropDown.js
@@ -5,10 +5,11 @@ import {ApplicationPaths} from "../../api-authorization/ApiAuthorizationConstant
 
 function ProfileDropDown() {
     return (
-        <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+        <DropdownButton id="dropdown-basic-button" title="Профиль">
             <Dropdown.Item href={ApplicationPaths.IdentityManagePath}>Профиль</Dropdown.Item>
             <Dropdown.Item href="/tariffs">Подписки</Dropdown.Item>
             <Dropdown.Item href="/models">Модели</Dropdown.Item>
+            <Dropdown.Item href={ApplicationPaths.LogOut}>Выйти</Dropdown.Item>
         </DropdownButton>
     );
 }
