66e3327dadd5eed0bf1ecd0dec002a04edc16e91
Arkirka <denis.vorobyov.00@gmail.com>
2022-10-12 23:06:47 +0300

    context & leaflet support added

--- WebClient/ClientApp/src/App.js
+++ WebClient/ClientApp/src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import { Layout } from './components/Layout';
+import { Layout } from './components/layout/Layout';
 import  Home  from './pages/home/Home.js'
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
 import ApiAuthorizationRoutes from './components/api-authorization/ApiAuthorizationRoutes';
 import { ApplicationPaths } from './components/api-authorization/ApiAuthorizationConstants';
-
 import './custom.css'
 import Tariffs from "./pages/Tariffs/Tariffs";
 import Models from "./pages/models/Models";
 import CreateModel from "./pages/CreateModel";
 import DashboardEmpty from "./pages/dashboardEmpty/DashboardEmpty";
+import MapMain from "./pages/map/MapMain";
+import Recommendations from "./pages/Recommendations";
 
 export default class App extends Component {
   static displayName = App.name;
 
   render () {
     return (
-        <>
             <Layout>
                 <Route exact path='/' component={Home } />
                 <AuthorizeRoute path={ApplicationPaths.IdentityManagePath} component={ApiAuthorizationRoutes} />
@@ -29,16 +29,12 @@ export default class App extends Component {
 
                 <Route path="/createModel" component={CreateModel} />
                 <Route path="/dashboardEmpty" component={DashboardEmpty} />
-                {/*<Route path="/map" component={MapMain} />
-                <Route path="/recom" component={Recommendations } />*/}
+                <Route path="/map" component={MapMain} />
+                <Route path="/recom" component={Recommendations } />
 
                 <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
                 <Route path="/models" component={Models } />
             </Layout>
-
-
-        </>
-
     );
   }
 }
