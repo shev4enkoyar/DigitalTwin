0ba680aa5be6a598ab1aa82c279bf1520ebaf443
Arkirka <denis.vorobyov.00@gmail.com>
2022-10-13 16:59:28 +0300

    dropdown refactored

--- WebClient/ClientApp/src/components/navMenu/components/ProfileDropDown.js
+++ WebClient/ClientApp/src/components/navMenu/components/ProfileDropDown.js
@@ -2,14 +2,20 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import React from "react";
 import {ApplicationPaths} from "../../api-authorization/ApiAuthorizationConstants";
+import {Link} from "react-router-dom";
+import {NavLink} from "reactstrap";
 
-function ProfileDropDown() {
+function ProfileDropDown(props) {
     return (
         <DropdownButton id="dropdown-basic-button" title="Профиль">
             <Dropdown.Item href={ApplicationPaths.IdentityManagePath}>Профиль</Dropdown.Item>
             <Dropdown.Item href="/tariffs">Подписки</Dropdown.Item>
             <Dropdown.Item href="/models">Модели</Dropdown.Item>
-            <Dropdown.Item href={ApplicationPaths.LogOut}>Выйти</Dropdown.Item>
+            <Dropdown.Item >
+                <NavLink tag={Link} to={props.logoutPath}>
+                    Выйти
+                </NavLink>
+            </Dropdown.Item>
         </DropdownButton>
     );
 }
