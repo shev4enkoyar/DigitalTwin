200674597fb971e983d688fafab2db3153f0233a
Arkirka <denis.vorobyov.00@gmail.com>
2022-10-18 14:04:30 +0300

    company create added

---
+++ WebClient/ClientApp/src/pages/createCompany/CreateCompany.js
@@ -0,0 +1,45 @@
+import React, {Component} from "react";
+import {Button, Container, Row} from "react-bootstrap";
+import Input from "../../components/input/Input";
+import CardForBody from "../../components/cardForBody/CardForBody";
+import authService from "../../components/api-authorization/AuthorizeService";
+
+export class CreateCompany extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { name: "", inn: "", supervisorName: "", contractId: "", error: ""};
+    }
+
+    render() {
+        return (
+          <Container>
+              <CardForBody>
+                  <p style={{color: "red"}}>{this.state.error}</p>
+                  <Input Label="Название комании" classNameP="textForSign12" className="inpCreateForDashCard" contClass="contForInpDashE" onInput={(event) => { let reg = /^[0-9A-Za-zА-Яа-я]*$/i.test(event.target.value); if (reg) this.setState({ name: event.target.value.trim() }) }} />
+                  <Input Label="ИНН" classNameP="textForSign12" className="inpCreateForDashCard" contClass="contForInpDashE" onInput={(event) => { let reg = /^[0-9A-Za-zА-Яа-я]*$/i.test(event.target.value); if (reg) this.setState({ inn: event.target.value.trim() }) }} />
+                  <Input Label="Имя руководителя" classNameP="textForSign12" className="inpCreateForDashCard" contClass="contForInpDashE" onInput={(event) => { let reg = /^[0-9A-Za-zА-Яа-я]*$/i.test(event.target.value); if (reg) this.setState({ supervisorName: event.target.value.trim() }) }} />
+                  <Input Label="ID контракта" classNameP="textForSign12" className="inpCreateForDashCard" contClass="contForInpDashE" onInput={(event) => { this.setState({ contractId: event.target.value.trim() }) }} />
+                  <Button onInput={this.createCompany}>
+                      Зарегистрировать компанию
+                  </Button>
+              </CardForBody>
+          </Container>
+        );
+    }
+
+    async createCompany() {
+        if (this.state.name === "" && this.state.inn === "" && this.state.supervisorName === "" && this.state.contractId === ""){
+            this.setState({error: "Одно или несколько полей пусто"});
+            return "error"
+        }
+        const token = await authService.getAccessToken();
+        await fetch(`api/company/create?name=${this.state.name}&inn=${this.state.inn}&supervisorName=${this.state.supervisorName}&contractId=${this.state.contractId}`, {
+            headers: !token ? {} :
+                {
+                    'Authorization': `Bearer ${token}`,
+                }
+        });
+        this.setState({error: "Компания создана"});
+    }
+}
---
+++ WebClient/Controllers/CompanyController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Linq;
+using WebClient.Data;
+using WebClient.Models;
+
+namespace WebClient.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/company")]
+    public class CompanyController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CompanyController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("create")]
+        public async Task<IActionResult> CreateCompany(string name, string inn, string supervisorName, string contractId) 
+        {
+            if (_dbContext.Companies.Any(x => x.CompanyINN == inn))
+            {
+                return BadRequest("Company already exists!");
+            }
+            Company createdCompany = new Company()
+            {
+                CompanyName = name,
+                CompanyINN = inn,
+                SupervisorName = supervisorName,
+                ContractId = contractId
+            };
+
+            await _dbContext.Companies.AddAsync(createdCompany);
+            return Ok();
+        }
+    }
+}
