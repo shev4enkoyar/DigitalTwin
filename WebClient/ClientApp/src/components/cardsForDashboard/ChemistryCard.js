0f5b60eaf3c52c660db165f1b597ad64d0c9e93c
Nasstyushka1312 <chicken1312@mail.ru>
2022-10-19 19:29:10 +0300

    added Combobox

--- WebClient/ClientApp/src/components/cardsForDashboard/ChemistryCard.js
+++ WebClient/ClientApp/src/components/cardsForDashboard/ChemistryCard.js
@@ -1,13 +1,35 @@
 ﻿import './CardsForDashboard.css';
-import { Container,Button } from 'react-bootstrap';
+import { Container,Button, Col, Row } from 'react-bootstrap';
 import BaseCard from "./BaseCard";
+import Combobox from '../combobox/ComboBox';
 const ChemistryCard = (props) => {
+    const chem = ["Выберите хим.средство...", "аа", "=="]
+    const pest = ["Выберите пестициды...", "аа"]
+    const ud = ["Выберите удобрения...", "==", "аа"]
+    const xxx = ["Выберите тип почвы ...", "аа"]
+    const isFull = () => {
+        if (props.values === null || props.values === undefined) {
+        }
+        return !Object.keys(props.values).map(val => props.values[val]).some(function (value, index, array) { return (value === false || value === 0 || value === "" || value === undefined) })
+    }
     return (
         <BaseCard visible={props.visible} off={props.off} hText="Статус модели" descr="Требуется добавить данные о химических средствах!" notifyColor="#DC3545" >
-            <Container className="contButton">
-                <Button className="btn btn-primary my-2" style={{ width: "190px" }} onClick={() => { if (props.values.chem) props.onClick() }}>
-                    Далее
-                </Button>
+            <Container className="contButton flex-wrap mx-3" style={{ width:'fit-content' }}>
+                <Row>
+                    <Col className="px-1">
+                        <Combobox className="FormControlSelect" classTextCombobox="textForSign12" textCombobox="Средство хим. защиты" classNameCont="padCombobox " options={chem} onChange={(empty) => { props.setStatus({ chem: empty }) }} />
+                        <Combobox className="FormControlSelect" classTextCombobox="textForSign12" textCombobox="Пестициды" classNameCont="padCombobox " options={pest} onChange={(empty) => { props.setStatus({pest: empty }) }} />
+                    </Col>
+                    <Col className="px-1">
+                        <Combobox className="FormControlSelect" classTextCombobox="textForSign12" textCombobox="Удобрения" classNameCont="padCombobox " options={ud} onChange={(empty) => { props.setStatus({ ud: empty }) }} />
+                        <Combobox className="FormControlSelect" classTextCombobox="textForSign12" textCombobox="Тип почвы" classNameCont="padCombobox " options={xxx} onChange={(empty) => { props.setStatus({ xxx: empty }) }} />
+                    </Col>
+                </Row>
+                <Row>
+                    <Button className="btn btn-primary my-2" style={{ width: "190px" }} onClick={() => { if (isFull() == true) props.onClick(); console.log(isFull()) }}>
+                        Далее
+                    </Button>
+                </Row>
             </Container>
         </BaseCard>
     )
