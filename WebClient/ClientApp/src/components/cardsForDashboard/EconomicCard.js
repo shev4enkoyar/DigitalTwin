7b1f9528de0962462c35b031a3ee2c1c44043dd0
Nasstyushka1312 <chicken1312@mail.ru>
2022-10-20 01:21:55 +0300

    added card CreateModel

--- WebClient/ClientApp/src/components/cardsForDashboard/EconomicCard.js
+++ WebClient/ClientApp/src/components/cardsForDashboard/EconomicCard.js
@@ -1,6 +1,7 @@
 ﻿import './CardsForDashboard.css';
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Row,Col } from 'react-bootstrap';
 import BaseCard from "./BaseCard";
+import Input from "./../input/Input";
 import React, {useState} from "react";
 import authService from "../api-authorization/AuthorizeService";
 import axios from "axios";
@@ -16,9 +17,22 @@ const EconomicCard = (props) => {
         });
         return await response.text();
     }
+    const isFull = () => {
+        if (this.props.values === null || this.props.values === undefined) {
+        }
+        return !Object.keys(this.props.values).map(val => this.props.values[val]).some(function (value, index, array) { return (value === false || value === 0 || value === "" || value === undefined) })
+    }
     return (
         <BaseCard visible={props.visible} hText="Статус модели" descr="Требуется добавить данные экономических показателей!" notifyColor="#DC3545">
+            <Col>
+                <Input Label="Наименование работы по высеву" classNameP="textForSign12" className="inpCreateForDashCard" contClass="contForInpDashE" value={props.values.vys} onInput={(event) => { props.setStatus({ vys: event.target.value.trim() }) }} />
+                <Input Label="Наименование работы по обработке" classNameP="textForSign12" className="inpCreateForDashCard" contClass="contForInpDashE" value={props.values.obr} onInput={(event) => {props.setStatus({ obr: event.target.value.trim() }) }} />
+                <Input Label="Наименование работы по сбору урожая" classNameP="textForSign12" className="inpCreateForDashCard" contClass="contForInpDashE" value={props.values.sbor} onInput={(event) => { props.setStatus({ sbor: event.target.value.trim() }) }} />
+                <Input Label="Длительность (период)" classNameP="textForSign12" className="inpCreateForDashCard" contClass="contForInpDashE" value={props.values.period} onInput={(event) => { props.setStatus({ period: event.target.value.trim() }) }} />
+                <Input Label="Работник на га" classNameP="textForSign12" className="inpCreateForDashCard" contClass="contForInpDashE" value={props.values.workerN} onInput={(event) => { var reg = /^([0-9]*)$/i.test(event.target.value); if (reg) props.setStatus({ workerN: event.target.value.trim() }) }} />
+            </Col>
             <Button className="btn btn-primary my-2" style={{ width: "190px" }} onClick={() => {
+                if (isFull() == true) props.onClick();
                 let digitalModel = {
                     Name: props.data.at(0).name,
                     UserId: -1,
@@ -26,7 +40,8 @@ const EconomicCard = (props) => {
                 };
                 console.log(CreateTechCard(digitalModel));
             }}>
-                <a style={{ color: "#fff" }} href={'/models'}>
+                <a style={{
+                    color: "#fff", textDecoration: 'none'}} href="/models">
                     Создать модель
                 </a>
             </Button>
