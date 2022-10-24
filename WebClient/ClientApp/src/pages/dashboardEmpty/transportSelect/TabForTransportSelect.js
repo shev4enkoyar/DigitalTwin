878d7031e0d329094c5b8de050dacefd1420534f
Nasstyushka1312 <chicken1312@mail.ru>
2022-10-25 01:01:58 +0300

    created modal window

--- WebClient/ClientApp/src/pages/dashboardEmpty/transportSelect/TabForTransportSelect.js
+++ WebClient/ClientApp/src/pages/dashboardEmpty/transportSelect/TabForTransportSelect.js
@@ -1,44 +1,42 @@
-import { Row, Col, Container} from 'react-bootstrap';
+import { Row, Col, Container, Button} from 'react-bootstrap';
 import './TransportSelect.css';
 import React from "react";
 import Combobox from './../../../components/combobox/ComboBox';
 import Input from './../../../components/input/Input';
-import ButtonEdit from '../../../components/button/ButtonEdit';
-import TableForTariffs from '../../../components/tableForTariffs/TableForTariffs';
+import TableForTariffs from './../../Tariffs/components/tableForTariffs/TableForTariffs';
 import { useState } from 'react';
 const TabForTransportSelect = (props) => {
     const transport = ["Выберите транспорт...", "Трактор",]
     const markaTrans = ["Выберите марку транспорта...", "1213",]
-    const markaSHin = ["Выберите марку шин...", "1213",]
     const [useAddTransp, setUseAddTransp] = useState
     (
-        { transp: 0, markTransp: 0, countTransp: 0, markShin: 0, countShin: 0 }
+        { transp: "Выберите транспорт...", markTransp: "Выберите марку транспорта...", countTransp: ""}
     )
-    const [isAddTransp, setAddTransp] = useState([])
     const handleSelectTransp = (value) =>
     {
-        setUseAddTransp({ ...useAddTransp, ...value })
+        const prev = useAddTransp;
+        setUseAddTransp({ ...prev, ...value })
+        console.log(useAddTransp)
     }
     const handleAddingTransp = () =>
     {
-        setAddTransp([...isAddTransp, { num: (isAddTransp ? isAddTransp.length : 0) + 1, ...useAddTransp }])
-        isAddTransp.map((value, index) => console.log({ num: index, ...value }))
+        const prev = props.values;
+        props.setStatus([...prev, { num: (prev ? prev.length : 0) + 1, ...useAddTransp }])
     }
-    const zagForTransp =['#', 'Наименование транспорта', 'Марка транспорта', 'Количество транспорта', 'Марка шин', 'Количество шин'];
+    const zagForTransp =['#', 'Наименование транспорта', 'Марка транспорта', 'Количество транспорта'];
     return (
         <Container className="tabForTransp">
-            <Row id="rowForTab">
-                <Combobox setInherit={(value) => { handleSelectTransp({ transp: value }) }} className="FormControlSelect" classTextCombobox="textForSign12" textCombobox="Транспорт" classNameCont="forTransp " options={transport} />
-                <Combobox setInherit={(value) => { handleSelectTransp({ markTransp: value }) }} className="FormControlSelect" classTextCombobox="textForSign12" textCombobox="Марка транспорта" classNameCont="forTransp " options={markaTrans} />
-                <Input onChange={(value) => { handleSelectTransp({ countTransp: value.target.value }) }} Label="Количество транспорта" classNameP="textForSign12 forNowrap" className="inpCreateForDashCard " contClass="inpContForTransp" placeholder="0" />
-                <Combobox setInherit={(value) => { handleSelectTransp({ markShin: value }) }} className="FormControlSelect"  classTextCombobox="textForSign12" textCombobox="Марка шин" classNameCont="forTransp " options={markaSHin} />
-                <Input onChange={(value) => { handleSelectTransp({ countShin: value.target.value }) }} Label="Количество шин" classNameP="textForSign12 forNowrap" className="inpCreateForDashCard " contClass="inpContForTransp" placeholder="0" />
+            <Row id="rowForTab" className="d-flex justify-content-center">
+                <Combobox onChange={(value) => { handleSelectTransp({ transp: value }) }} className="FormControlSelect" classTextCombobox="textForSign12" textCombobox="Транспорт" classNameCont="forTransp px-1" options={transport} />
+                <Combobox onChange={(value) => { handleSelectTransp({ markTransp: value }) }} className="FormControlSelect" classTextCombobox="textForSign12" textCombobox="Марка транспорта" classNameCont="forTransp px-1" options={markaTrans} />
+                <Input value={useAddTransp.countTransp} onInput={(event) => { var reg = /^[0-9]*$/i.test(event.target.value); if (reg) handleSelectTransp({ countTransp: event.target.value.trim() }) }} Label="Количество транспорта" classNameP="textForSign12 forNowrap" className="inpCreateForDashCard" contClass="inpContForTransp  px-1" placeholder="0" />
             </Row>
-            <Col style={{ marginBottom: '40px', display: 'flex', flexDirection:'column' }}>
-                <ButtonEdit onClick={handleAddingTransp} className="blueBut dashBut simpleBut" textForButton="Добавить" classTextName="textOpenSans14" buttonStyle={{margin:'10px 0px'} } />
-                <ButtonEdit onClick={() => { props.onClick(false) }} className="blueBut dashBut simpleBut" textForButton="Закрыть окно" classTextName="textOpenSans14" buttonStyle={{ margin: '5px 0px' }} />  
+            <Col className="d-flex flex-column align-items-center" style={{ marginBottom: '40px' }}>
+                <Button onClick={() => { console.log(useAddTransp); if (!(useAddTransp.transp == "Выберите транспорт...") && !(useAddTransp.markTransp == "Выберите марку транспорта...") && !(useAddTransp.countTransp == "")) handleAddingTransp() }} className="btn" style={{ margin: '10px 0px' }}>
+                    Добавить
+                </Button>
             </Col>
-            <TableForTariffs classNameTab="margTable" textForTable="Добавленный транспорт" classNamesTD="ForBox" headersForTable={zagForTransp} contentsForTable={(isAddTransp[0] === undefined) ? [{ num: "", transp: "", markaTrans: "", countTransp: "", markaSHin: "", countShin: "" }] : isAddTransp} />
+            <TableForTariffs classNameTab="margTable" textForTable="Добавленный транспорт" classNamesTD="ForBox" headersForTable={zagForTransp} contentsForTable={(props.values.length === 0) ? [{ num: "", transp: "", markaTrans: "", countTransp: "" }] : props.values} />
         </Container>
 
     );
--- WebClient/ClientApp/src/pages/dashboardEmpty/transportSelect/TransportSelect.js
+++ WebClient/ClientApp/src/pages/dashboardEmpty/transportSelect/TransportSelect.js
@@ -1,27 +1,29 @@
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import './TransportSelect.css';
-import { Container } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 import PopUpWithBlurCanvas from "../../../components/popUp/PopUpWithBlurCanvas";
 import TabForTransportSelect from './TabForTransportSelect';
 const TransportSelect = (props) => {
     return (
-        <PopUpWithBlurCanvas isBlur={true} isActive={props.isActive} handleActiveChange={props.handleActiveChange} className="TranspCard" styleFlex={{ top: '0px!important', marginTop:'0px' } }>
-            <Container style={{ padding: '0px'} }>
-            <Tabs
-                defaultActiveKey="first"
-                id="justify-tab-example"
-                className="mb-3"
-                justify>
-                <Tab eventKey="first" title="Транспорт для засева" >
-                    <TabForTransportSelect onClick={props.handleActiveChanged} />
-                </Tab>
-                <Tab eventKey="second" title="Транспорт для обработки" >
-                    <TabForTransportSelect onClick={props.handleActiveChanged} />
-                </Tab>
-                <Tab eventKey="third" title="Транспорт для сбора" >
-                    <TabForTransportSelect onClick={props.handleActiveChanged} />
-                </Tab>
+        <PopUpWithBlurCanvas isBlur={true} isActive={props.values.isActive} handleActiveChange={() => { props.setStatus({isActive:false})} } className="TranspCard" styleFlex={{ top: '0px!important', marginTop:'0px', margin:'auto' } } >
+            <Container style={{ padding: '0px' }}>
+                <Button onClick={() => { props.setStatus({ isActive: false }) }} className="btn btn-danger position-absolute" style={{right: '3px',top: '2px' }}>
+                    x
+                </Button>
+                <Tabs
+                    defaultActiveKey="first"
+                    id="justify-tab-example"
+                    className="mb-3 bg-transparent d-flex justify-content-center border-0">
+                    <Tab tabClassName="bg-primary text-white" eventKey="first" title="Транспорт для засева">
+                        <TabForTransportSelect onClick={() => { props.setStatus({ isActive: false }) }} setStatus={(v) => { props.setStatus({ zasev: v }) }} values={props.values.zasev} />
+                    </Tab>
+                    <Tab tabClassName="bg-primary text-white" eventKey="second" title="Транспорт для обработки">
+                        <TabForTransportSelect onClick={() => { props.setStatus({ isActive: false }) }} setStatus={(v) => { props.setStatus({ obrabotka: v }) }} values={props.values.obrabotka} />
+                    </Tab>
+                    <Tab tabClassName="bg-primary text-white" eventKey="third" title="Транспорт для сбора">
+                        <TabForTransportSelect onClick={() => { props.setStatus({ isActive: false }) }} setStatus={(v) => { props.setStatus({ sbor: v }) }} values={props.values.sbor} />
+                    </Tab>
                 </Tabs>
             </Container>
         </PopUpWithBlurCanvas>
