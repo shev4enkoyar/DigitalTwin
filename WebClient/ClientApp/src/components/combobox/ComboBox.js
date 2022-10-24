66e48ce042b43818e41a161771f02d5c3fd223ff
Nasstyushka1312 <chicken1312@mail.ru>
2022-10-25 01:30:04 +0300

    changed classes Combobox

--- WebClient/ClientApp/src/components/combobox/ComboBox.js
+++ WebClient/ClientApp/src/components/combobox/ComboBox.js
@@ -8,7 +8,7 @@ function Combobox(props) {
     }
     return (
         <>
-            <Container className={"p-0 form-group contForCombobox " + props.classNameCont}>
+            <Container className={"form-group contForCombobox " + props.classNameCont}>
                 <label className={props.classTextCombobox} >
                     {props.textCombobox}
                 </label>
