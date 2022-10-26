9a6f0d7ae3b5b92398807f206b4c799e1fbd6b79
Nasstyushka1312 <chicken1312@mail.ru>
2022-10-26 18:30:41 +0300

    chaged classes ContainerBlur

--- WebClient/ClientApp/src/pages/home/components/ContainerBlur.js
+++ WebClient/ClientApp/src/pages/home/components/ContainerBlur.js
@@ -1,20 +1,13 @@
 import '../Home.css';
 import { Container } from 'react-bootstrap';
 import React from 'react';
-import { ThemeContextConsumer } from "../../../components/ThemeContext"
 function ContainerBlur(props) {
 
 
     return (
-        <ThemeContextConsumer>
-            {
-                context=>(
                     <Container className="blurContainer">
                         {props.children}
                     </Container>
-                )
-            }
-        </ThemeContextConsumer>
     );
 }
 
