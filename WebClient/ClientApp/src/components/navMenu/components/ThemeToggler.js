f21d3b774effa432752fdd605a04ad425d5d7edb
Nasstyushka1312 <chicken1312@mail.ru>
2022-10-26 18:30:05 +0300

    created ThemeToggler

---
+++ WebClient/ClientApp/src/components/navMenu/components/ThemeToggler.js
@@ -0,0 +1,14 @@
+﻿import { Button } from 'react-bootstrap';
+import { ThemeContextConsumer } from "../../ThemeContext";
+function VectorIcon(props) {
+    return (
+        <ThemeContextConsumer>
+            {context => 
+            (
+                <Button style={{ backgroundColor: 'transparent', border: 'none' }} className={context.theme + (props.Gray ? "Gray " : " ") + "shadow-none"} onClick={context.toggleTheme}>
+                    <img fill="#000000" src="https://www.svgrepo.com/show/71693/sun-bright.svg" className={context.theme + (props.Gray ? "Gray " : "Icon ")} style={{ filter: 'invert(1)', width: '25px', height:'25px' }} />
+                </Button>
+            )}
+        </ThemeContextConsumer>
+    )
+} export default VectorIcon;
