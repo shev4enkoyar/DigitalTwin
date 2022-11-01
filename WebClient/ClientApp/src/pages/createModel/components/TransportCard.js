309d8c1adf63f905a5a31febdf6c459f8161cdc3
Nasstyushka1312 <chicken1312@mail.ru>
2022-11-01 21:36:20 +0300

    added BaseCard class & button back & skip

--- WebClient/ClientApp/src/pages/createModel/components/TransportCard.js
+++ WebClient/ClientApp/src/pages/createModel/components/TransportCard.js
@@ -1,8 +1,7 @@
 ﻿import './CardsForDashboard.css';
 import React from "react";
 import BaseCard from "./BaseCard";
-import {Container} from "reactstrap";
-import {Button} from "reactstrap/lib";
+import { Button, Container } from "reactstrap/lib";
 const TransportCard = (props) => {
     const warning = () => {
         if ((props.values.zasev.length > 0) && (props.values.obrabotka.length > 0) && (props.values.sbor.length > 0)) {
@@ -16,24 +15,20 @@ const TransportCard = (props) => {
         }
     }
     return (
-        <BaseCard visible={props.visible} off={props.off} hText="Статус тех.карты" descr={warning()} notifyColor="#DC3545" >
-            <Container style={{ display: 'flex', justifyContent: 'center', padding: '0px', margin: '0px 0px 0.5rem 0px', width: '100%' }}>
-                <Button onClick={() => { this.props.Back() }} className="btn btn-primary my-2" style={{ width: "190px" }} >
-                Назад
-                </Button>
-            </Container>
+        <BaseCard className="widForCardWithTable" visible={props.visible} off={props.off} hText="Статус тех.карты" descr={warning()} notifyColor="#DC3545" >
             <Container style={{ display: 'flex', justifyContent: 'center', padding: '0px', margin: '0px 0px 0.5rem 0px', width: '100%' }}>
                 <Button onClick={() => { props.setStatus({isActive:true}) }} className="btn btn-primary my-2" style={{ width: "190px" }} >
                     Выбрать
                 </Button>
             </Container>
-            <Container className="contButton">
-                <Button className="btn btn-primary my-2" style={{ width: "190px" }} onClick={() => {props.onClick() }}>
+            <Container className="contButtonBCard">
+                <Button onClick={() => { props.Back() }} className="btn btn-primary m-2" style={{ width: "25%" }} >
+                    Назад
+                </Button>
+                <Button className="btn btn-primary m-2" style={{ width: "32%" }} onClick={() => { props.onClick() }}>
                     Пропустить
                 </Button>
-            </Container>
-            <Container className="contButton">
-                <Button className="btn btn-primary my-2" style={{ width: "190px" }} onClick={() => { console.log(props.values.zasev.length, props.values.obrabotka.length, props.values.sbor.length); if ((props.values.zasev.length>0) && (props.values.obrabotka.length>0) && (props.values.sbor.length>0)) props.onClick() }}>
+                <Button className="btn btn-primary m-2" style={{ width: "25%" }} onClick={() => { console.log(props.values.zasev.length, props.values.obrabotka.length, props.values.sbor.length); if ((props.values.zasev.length>0) && (props.values.obrabotka.length>0) && (props.values.sbor.length>0)) props.onClick() }}>
                     Далее
                 </Button>
             </Container>
