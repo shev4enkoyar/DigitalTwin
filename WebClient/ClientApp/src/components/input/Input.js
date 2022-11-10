641a44351e3d560b6be5c7e2acc4fc47dc6fe984
Nasstyushka1312 <chicken1312@mail.ru>
2022-11-10 20:29:00 +0300

    added container & props for hints

--- WebClient/ClientApp/src/components/input/Input.js
+++ WebClient/ClientApp/src/components/input/Input.js
@@ -4,9 +4,12 @@ import './Input.css';
 function Input(props) {
     return (
         <Container className={props.contClass + " px-0"} style={props.styleContainer} hidden={props.hidden}>
-            <p className={props.classNameP}>
-                {props.Label}
-            </p>
+            <Container className={props.classNameContP + " p-0 m-0 flex-wrap d-flex align-items-baseline"} style={{ width: 'max-content',maxWidth: '100%' } }>
+                <p className={props.classNameP}>
+                    {props.Label}
+                </p>
+                {props.children}
+            </Container>
             <input onChange={props.onChange} onInput={props.onInput} value={props.value} className={props.className} placeholder={props.placeholder}/>
         </Container>
 
