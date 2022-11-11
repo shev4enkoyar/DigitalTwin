897b72146ab1da0853b5cbf986f99e346ae8235b
Arkirka <denis.vorobyov.00@gmail.com>
2022-11-12 02:13:58 +0300

    add soil Mosture for product

---
+++ Microservices/Microservice.DashboardManager/Migrations/20221111224712_SoilMostureAdded.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Microservice.DashboardManager.Migrations
+{
+    public partial class SoilMostureAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SoilMoistureMax",
+                table: "Products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SoilMoistureMin",
+                table: "Products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SoilMoistureMax",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "SoilMoistureMin",
+                table: "Products");
+        }
+    }
+}
