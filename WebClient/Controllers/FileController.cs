0eb57f32818e3bfb00b805654f7dd2cda2f08b6c
Yaroslav Shevchenko <schev4enko3@gmail.com>
2022-11-17 01:04:01 +0300

    Added file controller

---
+++ WebClient/Controllers/FileController.cs
@@ -0,0 +1,58 @@
+﻿using Microservice.FileManager.Protos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebClient.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        public async Task<IEnumerable<PageFileProto>> GetAllPagesAsync(int modelId, string sectionName)
+        {
+            HttpClient client = MicroservicesIP.GatewayHttpClient;
+
+            IEnumerable<PageFileProto> result = null;
+            HttpResponseMessage response = await client.GetAsync($"api/file/get_pages?modelId={modelId}&sectionName={sectionName}");
+            if (response.IsSuccessStatusCode)
+            {
+                var json = response.Content.ReadAsStringAsync().Result;
+                result = JsonConvert.DeserializeObject<IEnumerable<PageFileProto>>(json);
+            }
+            return result;
+        }
+
+        [HttpGet("download/document/{guidFile}")]
+        public IActionResult DownloadDocument(string guidFile, string extension)
+        {
+            string windowsPath = @"C:\ProgramData\document";
+            string linuxPath = @"/root/Project/Files/documents";
+            byte[] byteArray = GetDocumentByteArray(Path.Combine(windowsPath, guidFile + extension));
+            if (byteArray == null)
+                return BadRequest();
+
+            return new FileContentResult(byteArray, "application/octet-stream");
+        }
+
+        private byte[] GetDocumentByteArray(string file)
+        {
+            if (!System.IO.File.Exists(file))
+                return null;
+
+            try
+            {
+                return System.IO.File.ReadAllBytes(file);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
