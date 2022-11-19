f7d8f80ce390dd0bff5f5e1edf693bc99161b7d2
Yaroslav Shevchenko <schev4enko3@gmail.com>
2022-11-19 23:24:37 +0300

    Refactored

--- WebClient/Controllers/FunctionalController.cs
+++ WebClient/Controllers/FunctionalController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.ComponentModel.Design;
 using System.Linq;
 using System.Net.Http;
 using System.Security.Claims;
@@ -20,14 +19,14 @@ namespace WebClient.Controllers
     [Route("api/[controller]")]
     public class FunctionalController : CustomControllerBase
     {
-        private readonly UserManager<ApplicationUser> _userManager;
         private readonly ApplicationDbContext _dbContext;
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<ApplicationRole> _roleManager;
 
         public FunctionalController(UserManager<ApplicationUser> userManage, ApplicationDbContext dbContext, RoleManager<ApplicationRole> roleManager)
         {
-            _userManager = userManage;
             _dbContext = dbContext;
+            _userManager = userManage;
             _roleManager = roleManager;
         }
 
@@ -38,40 +37,51 @@ namespace WebClient.Controllers
             //TODO Yarick, imagine something better pls
             if (user == null)
                 return null;
-            var userRoles = _dbContext.UserRoles.Where(x => x.UserId == user.Id).ToList();
+            var userRoles = _dbContext.UserRoles
+                .Where(x => x.UserId == user.Id)
+                .ToList();
 
             HashSet<int> functionalAccessIds = new HashSet<int>();
-            userRoles.Select(x => _roleManager.FindByIdAsync(x.RoleId).Result.FunctionalAccess).ToList().ForEach(x => x.Split(";").ToList().ForEach( y => functionalAccessIds.Add(int.Parse(y)) ));
+            userRoles
+                .Select(x => _roleManager.FindByIdAsync(x.RoleId).Result.FunctionalAccess)
+                .ToList()
+                .ForEach(x => x.Split(";").ToList().ForEach(y => functionalAccessIds.Add(int.Parse(y))));
 
             List<string> functions = new List<string>();
             foreach (int functionalId in functionalAccessIds)
                 functions.Add(_dbContext.Functionals.Where(x => x.Id == functionalId).FirstOrDefault().Name);
 
             IEnumerable<int> modelIds = await GetDigitalModels();
-            if (modelIds == null) 
+            if (modelIds == null)
                 return functions;
 
             foreach (int modelId in modelIds)
             {
                 var functionalIds = await GetFunctionalIdsFromSubscriptions(modelId);
-                functionalAccessIds.Intersect(functionalIds).ToList().ForEach(x => functions.Add( $"{_dbContext.Functionals.Where(y => y.Id == x).FirstOrDefault().Name}/{modelId}"));
+                functionalAccessIds
+                    .Intersect(functionalIds)
+                    .ToList()
+                    .ForEach(x => functions.Add($"{_dbContext.Functionals.Where(y => y.Id == x).FirstOrDefault().Name}/{modelId}"));
             }
 
             return functions;
         }
 
-        private async Task<IEnumerable<int>> GetFunctionalIdsFromSubscriptions(int modelId) 
+        private async Task<IEnumerable<int>> GetFunctionalIdsFromSubscriptions(int modelId)
         {
             List<int> result = new List<int>();
             var subscriptions = await GetActivatedSubscription(modelId);
             if (subscriptions == null)
                 return null;
 
-            subscriptions.ToList().ForEach(x => x.FunctionalAccess.Split(";").ToList().ForEach(y => result.Add(int.Parse(y))));
+            subscriptions
+                .ToList()
+                .ForEach(x => x.FunctionalAccess.Split(";").ToList().ForEach(y => result.Add(int.Parse(y))));
+
             return result;
         }
 
-        private async Task<IEnumerable<SubscriptionClientProto>> GetActivatedSubscription(int modelId) 
+        private async Task<IEnumerable<SubscriptionClientProto>> GetActivatedSubscription(int modelId)
         {
             IEnumerable<SubscriptionClientProto> result = null;
             HttpResponseMessage response = await ConnectionClient.GetAsync($"api/subscription/get_activated_subscription/{modelId}");
