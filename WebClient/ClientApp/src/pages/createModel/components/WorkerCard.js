dd0d1cab07b71953affaf497fafe93c30470c609
Nasstyushka1312 <chicken1312@mail.ru>
2022-11-20 22:34:23 +0300

    deleted buttons

--- WebClient/ClientApp/src/pages/createModel/components/WorkerCard.js
+++ WebClient/ClientApp/src/pages/createModel/components/WorkerCard.js
@@ -1,8 +1,9 @@
 ﻿import './CardsForDashboard.css';
 import React from "react";
 import BaseCard from "./BaseCard";
-import {Container,Row } from "reactstrap";
-const TransportCard = (props) => {
+import { Container, Row } from "reactstrap";
+import authService from "../../../components/api-authorization/AuthorizeService";
+const WorkerCard = (props) => {
     const warning = () => {
         if ((props.values.rZasev.length > 0) && (props.values.rObrabotka.length > 0) && (props.values.rSbor.length > 0)) {
             return "Данные о работниках сохранены";
@@ -25,15 +26,26 @@ const TransportCard = (props) => {
                     <button onClick={() => { props.Back() }} className="btn btn-primary my-2 mr-2" style={{ width: "max-content" }} >
                         Назад
                     </button>
-                    <button className="btn btn-primary my-2 ml-2" style={{ width: "max-content" }} onClick={() => { console.log(props.values.rZasev.length, props.values.rObrabotka.length, props.values.rSbor.length); if ((props.values.rZasev.length > 0) && (props.values.rObrabotka.length > 0) && (props.values.rSbor.length > 0)) props.onClick() }}>
-                        Далее
-                    </button>
                 </Row>
-                <button className="btn btn-primary m-2" style={{ width: "max-content" }} onClick={() => { props.onClick() }}>
-                    Пропустить
+                <button className="btn btn-primary m-2" onClick={() => {
+                    let digitalModel = {
+                        Name: props.data.at(0).name,
+                        UserId: -1,
+                        ProductId: props.data.at(2).productId,
+                        Cadaster: props.data.at(3).kad,
+                        CategoryName: props.data.at(2).cult + " " + props.data.at(2).sort
+                    };
+                    CreateTechCard(digitalModel);
+                    props.onClick();
+                }}>
+                    <a style={{
+                        color: "#fff", textDecoration: 'none'
+                    }} href="/models">
+                        Создать модель
+                    </a>
                 </button>
             </Container>
         </BaseCard>
     )
 }
-export default TransportCard;
+export default WorkerCard;
