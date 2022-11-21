6ec82edc6f6beae835761e997f0fd0dbf3d93ea4
Yaroslav Shevchenko <schev4enko3@gmail.com>
2022-11-21 22:36:07 +0300

    Refactored

--- Microservices/Microservice.FileManager/DocumentCreator.cs
+++ Microservices/Microservice.FileManager/DocumentCreator.cs
@@ -1,23 +1,24 @@
-﻿using System;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
 using System.Collections.Generic;
-using CsvHelper;
 using System.Globalization;
 using System.IO;
 using System.Threading.Tasks;
-using CsvHelper.Configuration;
 
 namespace Microservice.FileManager
 {
     public static class DocumentCreator
     {
-        const string linuxPath = @"/root/Project/Files/documents";
-        private static string DocumentPath { get; } = linuxPath;
-
         public static async Task<string> CreateCsvDocument<T>(List<T> dataList)
         {
-            var csvConfig = new CsvConfiguration(new CultureInfo("ru-RU")) { HasHeaderRecord = false };
-            var filePath = Path.Combine(DocumentPath, $"{Guid.NewGuid()}.csv");
-            await using var streamWriter = File.CreateText($"{DocumentPath}/{Guid.NewGuid()}.csv");
+            var csvConfig = new CsvConfiguration(new CultureInfo("ru-RU"))
+            {
+                HasHeaderRecord = false
+            };
+
+            var filePath = Path.Combine("/", "root", "Project", "Files", "documents", $"{Guid.NewGuid()}.csv");
+            await using var streamWriter = File.CreateText(filePath);
             await using var csvWriter = new CsvWriter(streamWriter, csvConfig);
             {
                 await csvWriter.WriteRecordsAsync(dataList);
