41f575428fa3ac480fbf21618ab97d0a959ccf3d
Arkirka <denis.vorobyov.00@gmail.com>
2022-12-01 23:35:44 +0300

    change product table

---
+++ Microservices/Microservice.DashboardManager/Migrations/20221201201647_ChangeProduct.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Microservice.DashboardManager.Migrations
+{
+    public partial class ChangeProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TaskList",
+                table: "Products",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TaskList",
+                table: "Products");
+        }
+    }
+}
