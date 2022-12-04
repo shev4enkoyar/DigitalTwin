b2a81db9a8dcb3e8f546d1e9e150998110ada147
Nasstyushka1312 <chicken1312@mail.ru>
2022-12-04 18:45:17 +0300

    added query add worker

--- WebClient/ClientApp/src/pages/createModel/components/TabForWorkers.js
+++ WebClient/ClientApp/src/pages/createModel/components/TabForWorkers.js
@@ -1,15 +1,17 @@
-﻿import React, { useState } from "react";
+﻿import React, { useState, useEffect } from "react";
 import BootstrapTable from 'react-bootstrap-table-next';
 import { Button, Col, Container, Row } from "reactstrap/lib";
 import Combobox from '../../../components/combobox/ComboBox';
 import Input from '../../../components/input/Input';
 import { ThemeContextConsumer } from '../../../components/ThemeContext';
 import './CardsForDashboard.css';
+import authService from "./../../../components/api-authorization/AuthorizeService";
 const TabForWorkers = (props) => {
     const dol = ["Выберите должность...", "тракторист",]
+
     const [useAddWorker, setUseAddWorker] = useState
         (
-            { dol: "Выберите должность...", stavk: "", oklad: "", vak: "" }
+            { dol: -1, stavk: "", oklad: "", vak: "", workers: [] }
         )
     const handleSelectWorker = (value) => {
         const prev = useAddWorker;
@@ -19,6 +21,28 @@ const TabForWorkers = (props) => {
     const handleAddingWorker = () => {
         const prev = props.values;
         props.setStatus([...prev, { num: (prev ? prev.length : 0) + 1, ...useAddWorker }])
+            (async () => {
+                try {
+                    const token = await authService.getAccessToken();
+                    const response = await fetch(`api/worker/create?modelId=${props.}&postId=${postId}&fio=${fio}&rate=${rate}&salary=${salary}`, {
+                        headers: !token ? {} : { 'Authorization': `Bearer ${token}` }
+                    });
+                    console.log(33333);
+                    const data = await response.json();
+                    console.log(11111);
+                    console.log(data);
+                    let workerTemp = [];
+                    workerTemp.push({ id: -1, post: "Выберите должность..." });
+                    data.map(el => workerTemp.push(el));
+                    console.log(workerTemp);
+                    if (mounted) {
+                        handleSelectWorker({ workers: workerTemp });
+                    }
+
+                } catch (e) {
+                    console.log(e);
+                }
+            })();
     }
     const zagForWorker = [
         {
@@ -42,27 +66,61 @@ const TabForWorkers = (props) => {
             text: 'ФИО (либо наименование вакансии)'
         }
     ];
+    useEffect(() => {
+        let mounted = true;
+        (async () => {
+            try {
+                const token = await authService.getAccessToken();
+                const response = await fetch('api/worker/get_posts', {
+                    headers: !token ? {} : { 'Authorization': `Bearer ${token}` }
+                });
+                console.log(33333);
+                const data = await response.json();
+                console.log(11111);
+                console.log(data);
+                let workerTemp = [];
+                workerTemp.push({ id: -1, post: "Выберите должность..." });
+                data.map(el => workerTemp.push(el));
+                console.log(workerTemp);
+                if (mounted) {
+                    handleSelectWorker({ workers: workerTemp });
+                }
+                
+            } catch (e) {
+                console.log(e);
+            }
+        })();
+        return () => {
+            mounted = false;
+        };
+    }, []);
     return (
         <ThemeContextConsumer>
             {
                 context => (
-                        <Container className="tabForTransp">
-                            <Row id="rowForTab" className="d-flex justify-content-center">
-                                <Combobox onChange={(value) => { handleSelectWorker({ dol: value }) }} className="FormControlSelect" classTextCombobox="textForSign12" textCombobox="Должность" classNameCont="forTransp px-1" options={dol} />
-                                <Input value={useAddWorker.stavk} onInput={(event) => { var reg = /^(0|([1-9][0-9]{0,5}))?(\.|(\.[0-9]{1,5}))?$/i.test(event.target.value); if (reg) handleSelectWorker({ stavk: event.target.value.trim() }) }} Label="Ставка" classNameP="textForSign12 forNowrap" className="inpCreateForDashCard" contClass="inpContForTransp  px-1" placeholder="0" />
-                                <Input value={useAddWorker.oklad} onInput={(event) => { var reg = /^(0|([1-9][0-9]{0,5}))?(\.|(\.[0-9]{1,5}))?$/i.test(event.target.value); if (reg) handleSelectWorker({ oklad: event.target.value.trim() }) }} Label="Оклад" classNameP="textForSign12 forNowrap" className="inpCreateForDashCard" contClass="inpContForTransp  px-1" placeholder="0" />
-                                <Input value={useAddWorker.vak} onInput={(event) => { handleSelectWorker({ vak: event.target.value.trim() }) }} Label="ФИО (либо наименование вакансии)" classNameP="textForSign12 forNowrap" className="inpCreateForDashCard" contClass="inpContForTransp  px-1" placeholder="0" />
-                            </Row>
-                            <Col className="d-flex flex-column align-items-center" style={{ marginBottom: '40px' }}>
-                                <Button onClick={() => { console.log(useAddWorker); if (!(useAddWorker.dol == "Выберите транспорт...") && !(useAddWorker.stavk == "") && !(useAddWorker.oklad == "") && !(useAddWorker.vak == "")) handleAddingWorker() }} className="btn" style={{ margin: '10px 0px' }}>
-                                    Добавить
-                                </Button>
-                            </Col>
-                            <Container className="ContForHistoryTariff p-0 contForReactTable">
-                                <BootstrapTable classes={context.theme + " HistoryTableText"} keyField='num' data={(props.values.length === 0) ? [{ num: "", dol: "", stavk: "", oklad: "", vak: "" }] : props.values} columns={zagForWorker} />
-                            </Container>
-                            {/*<TableForTariffs classNameTab="margTable" textForTable="Добавленный транспорт" classNamesTD="ForBox" headersForTable={zagForWorker} contentsForTable={(props.values.length === 0) ? [{ num: "", dol: "", stavk: "", oklad: "", vak: """ }] : props.values} />*/}
+                    <Container className="tabForTransp">
+                        <Row id="rowForTab" className="d-flex justify-content-center">
+                            <Combobox className="FormControlSelect" classTextCombobox="textForSign12" textCombobox="Должность" classNameCont="forTransp px-1" options={useAddWorker.workers.map(el=>el.post)} 
+                                onChange={(empty) => {
+                                    const temp = useAddWorker.workers.find(el => el.post === empty);
+                                    if (temp) {
+                                        handleSelectWorker({dol:temp.post});
+                                    }
+                                }} />
+                            <Input value={useAddWorker.stavk} onInput={(event) => { var reg = /^(0|([1-9][0-9]{0,5}))?(\.|(\.[0-9]{1,5}))?$/i.test(event.target.value); if (reg) handleSelectWorker({ stavk: event.target.value.trim() }) }} Label="Ставка" classNameP="textForSign12 forNowrap" className="inpCreateForDashCard" contClass="inpContForTransp  px-1" placeholder="0" />
+                            <Input value={useAddWorker.oklad} onInput={(event) => { var reg = /^(0|([1-9][0-9]{0,5}))?(\.|(\.[0-9]{1,5}))?$/i.test(event.target.value); if (reg) handleSelectWorker({ oklad: event.target.value.trim() }) }} Label="Оклад" classNameP="textForSign12 forNowrap" className="inpCreateForDashCard" contClass="inpContForTransp  px-1" placeholder="0" />
+                            <Input value={useAddWorker.vak} onInput={(event) => { handleSelectWorker({ vak: event.target.value }) }} Label="ФИО (либо наименование вакансии)" classNameP="textForSign12 forNowrap" className="inpCreateForDashCard" contClass="inpContForTransp  px-1" placeholder="0" />
+                        </Row>
+                        <Col className="d-flex flex-column align-items-center" style={{ marginBottom: '40px' }}>
+                            <Button onClick={() => { console.log(useAddWorker); if (!(useAddWorker.dol == "Выберите должность...") && !(useAddWorker.stavk == "") && !(useAddWorker.oklad == "") && !(useAddWorker.vak == "")) handleAddingWorker() }} className="btn" style={{ margin: '10px 0px' }}>
+                                Добавить
+                            </Button>
+                        </Col>
+                        <Container className="ContForHistoryTariff p-0 contForReactTable">
+                            <BootstrapTable classes={context.theme + " HistoryTableText"} keyField='num' data={(props.values.length === 0) ? [{ num: "", dol: "", stavk: "", oklad: "", vak: "" }] : props.values} columns={zagForWorker} />
                         </Container>
+                        {/*<TableForTariffs classNameTab="margTable" textForTable="Добавленный транспорт" classNamesTD="ForBox" headersForTable={zagForWorker} contentsForTable={(props.values.length === 0) ? [{ num: "", dol: "", stavk: "", oklad: "", vak: """ }] : props.values} />*/}
+                    </Container>
                 )
             }
         </ThemeContextConsumer>
