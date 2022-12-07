43a15f59ff1417a8e9706c2f3d1fef33fc1a02af
Yaroslav Shevchenko <schev4enko3@gmail.com>
2022-12-07 22:12:42 +0300

    Fix cast

--- Microservices/Microservice.DashboardManager/BackgroundServices/ProductPriceUpdaterScopedService.cs
+++ Microservices/Microservice.DashboardManager/BackgroundServices/ProductPriceUpdaterScopedService.cs
@@ -119,7 +119,7 @@ namespace Microservice.DashboardManager.BackgroundServices
                 var productPriceObj = prop.GetValue(jsonData.Data.Rates, null);
                 decimal productPrice;
                 if (productPriceObj != null)
-                    productPrice = decimal.Parse(productPriceObj.ToString()[..(productPriceObj.ToString().Length <= 27 ? productPriceObj.ToString().Length : 28)]);
+                    productPrice = (decimal)double.Parse(productPriceObj.ToString());
                 else
                     continue;
 
