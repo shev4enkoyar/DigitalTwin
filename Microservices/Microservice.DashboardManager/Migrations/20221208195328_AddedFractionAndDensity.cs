3308c3791ddf7ae0d517a6eb96a189a9c8cd06aa
Yaroslav Shevchenko <schev4enko3@gmail.com>
2022-12-08 22:54:50 +0300

    Added fraction and density to dashboard model

---
+++ Microservices/Microservice.DashboardManager/Migrations/20221208195328_AddedFractionAndDensity.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Microservice.DashboardManager.Migrations
+{
+    public partial class AddedFractionAndDensity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Density",
+                table: "DigitalModels",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Fraction",
+                table: "DigitalModels",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Density",
+                table: "DigitalModels");
+
+            migrationBuilder.DropColumn(
+                name: "Fraction",
+                table: "DigitalModels");
+        }
+    }
+}
