66b7815318d0105992cae118c179130ced2b3f81
Arkirka <denis.vorobyov.00@gmail.com>
2022-12-09 16:47:05 +0300

    map controller added

---
+++ Gateway/Controllers/MapController.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Net.Client;
+using Microservice.MapManager.Protos;
+using Microsoft.AspNetCore.Mvc;
+using Shared;
+using System.Threading.Tasks;
+
+namespace Gateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MapController : ControllerBase
+    {
+        [HttpGet("get_area/{modelId}")]
+        public async Task<double> ValidateCadasterAsync(int modelId)
+        {
+            using var channel = GrpcChannel.ForAddress(MicroservicesIp.External.Map,
+                new GrpcChannelOptions { HttpHandler = SharedTools.GetDefaultHttpHandler }
+            );
+
+            var client = new MapService.MapServiceClient(channel);
+            var reply = await client.GetMapAreaAsync(new GetMapAreaRequest{ ModelId = modelId });
+
+            return reply.Area;
+        }
+    }
+}
---
+++ WebClient/Controllers/MapController.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Net.Client;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Shared;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebClient.Controllers.Base;
+
+namespace WebClient.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MapController : CustomControllerBase
+    {
+        [HttpGet("get_area/{modelId}")]
+        public async Task<double> ValidateCadasterAsync(int modelId)
+        {
+            var response = await ConnectionClient.GetAsync($"api/map/get_area/{modelId}");
+            if (!response.IsSuccessStatusCode)
+                return 0;
+            var json = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<double>(json);
+            return result;
+        }
+    }
+}
